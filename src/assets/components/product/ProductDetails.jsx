19a60dc1d2e6ba04e6737f40f3efc8b4b93f35e3
Adrianna Paluszak <adapaluszak@gmail.com>
2025-04-17 21:16:01 +0200

    react

---
+++ src/assets/components/product/ProductDetails.jsx
@@ -0,0 +1,8 @@
+const ProductDetails = (selectedProduct) => {
+if (!selectedProduct) {
+    return <p>Wybierz produkt żeby wyświetlić szczegóły</p>
+}
+    return <><p>Szczegóły</p></>
+}
+
+export default ProductDetails;
